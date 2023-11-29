a5315d68f56562fc1e684002eaad0ad7e43a79ca
ChenXr <warrior22@qq.com>
2023-11-29 18:14:31 +0800

    :zap: remove restart before bootstrap

--- src/rootserver/ob_root_service.cpp
+++ src/rootserver/ob_root_service.cpp
@@ -1084,9 +1084,11 @@ int ObRootService::start_service()
       FLOG_WARN("fail to set rs status", KR(ret));
     } else if (OB_FAIL(schedule_refresh_server_timer_task(0))) {
       FLOG_WARN("failed to schedule refresh_server task", KR(ret));
-    } else if (OB_FAIL(schedule_restart_timer_task(0))) {
-      FLOG_WARN("failed to schedule restart task", KR(ret));
-    } else if (OB_FAIL(schema_service_->get_ddl_epoch_mgr().remove_all_ddl_epoch())) {
+    } 
+    // else if (OB_FAIL(schedule_restart_timer_task(0))) {
+    //   FLOG_WARN("failed to schedule restart task", KR(ret));
+    // }
+     else if (OB_FAIL(schema_service_->get_ddl_epoch_mgr().remove_all_ddl_epoch())) {
       FLOG_WARN("fail to remove ddl epoch", KR(ret));
     } else if (debug_) {
       if (OB_FAIL(init_debug_database())) {
@@ -1858,8 +1860,8 @@ int ObRootService::after_restart()
   ObCurTraceId::init(GCONF.self_addr_);
 
   // avoid concurrent with bootstrap
-  // FLOG_INFO("[ROOTSERVICE_NOTICE] try to get lock for bootstrap in after_restart");
-  // ObLatchRGuard guard(bootstrap_lock_, ObLatchIds::RS_BOOTSTRAP_LOCK);
+  FLOG_INFO("[ROOTSERVICE_NOTICE] try to get lock for bootstrap in after_restart");
+  ObLatchRGuard guard(bootstrap_lock_, ObLatchIds::RS_BOOTSTRAP_LOCK);
 
   // NOTE: Following log print after lock
   FLOG_INFO("[ROOTSERVICE_NOTICE] start to do restart task");
@@ -5556,7 +5558,7 @@ int ObRootService::ObRefreshServerTask::process()
   const bool load_frozen_status = true;
   const bool need_retry = true;
   FLOG_INFO("refresh server task process");
-  // ObLatchRGuard guard(root_service_.bootstrap_lock_, ObLatchIds::RS_BOOTSTRAP_LOCK);
+  ObLatchRGuard guard(root_service_.bootstrap_lock_, ObLatchIds::RS_BOOTSTRAP_LOCK);
   if (OB_FAIL(root_service_.refresh_server(load_frozen_status, need_retry))) {
     FLOG_WARN("refresh server failed", K(ret), K(load_frozen_status));
   } else {}
