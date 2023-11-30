ac8060088fa087ffd11ea8490464cffedd33894e
ChenXr <warrior22@qq.com>
2023-11-30 13:51:21 +0800

    :sparkles: update deploy.py

--- deploy.py
+++ deploy.py
@@ -45,14 +45,14 @@ def __clear_env(cluster_home_path:str) -> None:
         path_to_clear = os.path.join(cluster_home_path, path)
         shutil.rmtree(path_to_clear, ignore_errors=True)
 
-def __gen_perf(cluster_home_path:str) -> None:
+def __gen_perf(cluster_home_path:str, sleep_time:int) -> None:
     observer_bin_path = __observer_bin_path(cluster_home_path)
     pid = subprocess.getoutput(f'pidof {observer_bin_path}')
     if pid:
-        _logger.info('start to gen perf.data. pid=%d, sleep=48')
+        _logger.info('start to gen perf.data, pid=%s, sleep=%ds', pid, sleep_time)
         # 定义一个新的函数来运行 subprocess.run
         def run_perf_command():
-            subprocess.run(f'perf record -F 99 -p {pid} -g -o /home/ChenXr/FlameGraph/perf.data -- sleep 48', shell=True)
+            subprocess.run(f'perf record -F 99 -p {pid} -g -o ./perf.data -- sleep {sleep_time}', shell=True)
 
 
         # 创建一个线程并启动它
@@ -99,6 +99,8 @@ def __create_tenant(cursor, *,
     # tenant create
     _logger.info(f'execute tenant create: {create_tenant_sql}')   
     tenant_create_begin = datetime.datetime.now()   
+    if args.perf_create_tenant:
+        __gen_perf(home_abs_path, 24)
     cursor.execute(create_tenant_sql)
     tenant_create_end = datetime.datetime.now()       
     _logger.info('execute tenant create done %s ms' % ((tenant_create_end - tenant_create_begin).total_seconds() * 1000))
@@ -116,7 +118,8 @@ if __name__ == "__main__":
     parser.add_argument("--cluster-home-path", dest='cluster_home_path', type=str, help="the path of sys log / config file / sql.sock / audit info")
     parser.add_argument("--only_build_env", action='store_true', help="build env & start observer without bootstrap and basic check")
     parser.add_argument('--clean', action='store_true', help='clean deploy directory and exit')
-    parser.add_argument('--perf', action='store_true', help='monitor the entire process and generate perf.data')
+    parser.add_argument('--perf_all', action='store_true', help='monitor the entire process and generate perf.data')
+    parser.add_argument('--perf_create_tenant', action='store_true', help='monitor the process of creating tenants and generate perf.data')
     parser.add_argument("-p", dest="mysql_port", type=str, default="2881")
     parser.add_argument("-P", dest="rpc_port", type=str, default="2882")
     parser.add_argument("-z", dest="zone", type=str, default="zone1")
@@ -157,8 +160,8 @@ if __name__ == "__main__":
     shell_result = subprocess.run(observer_cmd, shell=True)
     _logger.info('deploy done. returncode=%d', shell_result.returncode)
 
-    if args.perf:
-        __gen_perf(home_abs_path)
+    if args.perf_all:
+        __gen_perf(home_abs_path, 42)
 
     time.sleep(10)
     try:
