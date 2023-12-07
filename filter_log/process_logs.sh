1cafda72c292b282ea0db4376e82c69152d1b6c3
ChenXr <warrior22@qq.com>
2023-12-07 11:55:00 +0800

    :sparkles: update process_log.sh

--- filter_log/process_logs.sh
+++ filter_log/process_logs.sh
@@ -9,12 +9,15 @@ fi
 input_log="$1"
 output_sorted_file="$2"
 
+# 删除包含类似[work_job_xxxx]的行
+grep -v '\[work_job_[0-9]\{1,4\}\]' "$input_log" > ./temp_cleaned_log.log
+
 # 提取带有 "cost=" 的行到一个临时文件
-grep "cost=" "$input_log" > ./temp_cost_lines.log
+grep "cost=" "./temp_cleaned_log.log" > ./temp_cost_lines.log
 
 # 提取 cost 值，并排序
 awk -F'cost=' '{print $2, $0}' temp_cost_lines.log | sort -n -r | cut -d' ' -f2- > "$output_sorted_file"
 
 # 清理临时文件
-rm temp_cost_lines.log
+rm temp_cleaned_log.log temp_cost_lines.log
 
